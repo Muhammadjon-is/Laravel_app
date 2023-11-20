c1fc55a56ced452526a0e2f5db9a2231bd94dda4
Mukhammadjon <isakovw18@gmail.com>
2023-11-20 01:57:02 +0100

    fixed

--- resources/views/users/register.blade.php
+++ resources/views/users/register.blade.php
@@ -2,7 +2,7 @@
     <x-card class="p-10 max-w-lg mx-auto mt-24">
       <header class="text-center">
         <h2 class="text-2xl font-bold uppercase mb-1">Register</h2>
-        <p class="mb-4">Create an account to post gigs</p>
+        <p class="mb-4">Create an account to post job</p>
       </header>
   
       <form method="POST" action="/users">
